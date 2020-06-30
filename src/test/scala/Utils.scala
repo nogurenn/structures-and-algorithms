83fa5f4d3a8d62680eb0b1c66e27ea9f2fb78715
Glenn Liwanag <glennharveyliwanag@protonmail.com>
2020-06-30 21:08:59 +0800

    Add max value API

--- src/test/scala/Utils.scala
+++ src/test/scala/Utils.scala
@@ -2,5 +2,7 @@ import scala.util.Random
 
 trait Utils {
   // generate array of n length with random values [0, Int.MaxValue)
-  def generateRandArrayInt(n: Int = 50000): Array[Int] = Array.fill(n)(Random.nextInt(Int.MaxValue))
+  def generateRandArrayInt(n: Int = 50000): Array[Int] = generateRandArrayInt(n, Int.MaxValue)
+
+  def generateRandArrayInt(n: Int, maxValue: Int): Array[Int] = Array.fill(n)(Random.nextInt(maxValue))
 }
